da4d020d150b0b29e2fdf9571e0586341347d27b
Bill Fraser <bill@wfraser.dev>
2024-06-18 03:02:03 -0700

    add some helpers, rename some things

--- src/lib.rs
+++ src/lib.rs
@@ -17,32 +17,53 @@ pub use generated::MAP;
 /// Case-fold a character to lower-case, using the same rules as Dropbox uses for file paths.
 /// If the character is not an upper-case character according to the mapping, returns the original
 /// character unchanged.
-pub fn dbx_tolower(c: char) -> char {
+pub fn dbx_lowercase(c: char) -> char {
     MAP.binary_search_by(|(upper, _)| upper.cmp(&c))
         .map(|i| MAP[i].1)
         .unwrap_or(c)
 }
 
-/// Case-fold a string to lower-case. See [`dbx_tolower`].
-pub fn dbx_str_tolower(s: &str) -> String {
-    s.chars().map(dbx_tolower).collect()
+/// Case-fold a string to lower-case. See [`dbx_lowercase`].
+pub fn dbx_str_lowercase(s: &str) -> String {
+    s.chars().map(dbx_lowercase).collect()
+}
+
+/// Check whether two strings are equal, ignoring case. See [`dbx_lowercase`].
+pub fn dbx_eq_ignore_case(a: &str, b: &str) -> bool {
+    a.chars().map(dbx_lowercase).eq(b.chars().map(dbx_lowercase))
+}
+
+/// Returns a string slice with the prefix removed, ignoring case. See [`dbx_lowercase`] and
+/// [`str::strip_prefix`].
+pub fn dbx_strip_prefix_ignore_case<'a>(s: &'a str, prefix: &str) -> Option<&'a str> {
+    let mut pfx_it = prefix.chars().map(dbx_lowercase);
+    let mut last = None;
+    let trimmed = s.trim_start_matches(|c: char| {
+        last = pfx_it.next();
+        last == Some(dbx_lowercase(c))
+    });
+    if last.is_some() {
+        None
+    } else {
+        Some(trimmed)
+    }
 }
 
 #[cfg(test)]
 mod test {
-    use crate::{dbx_str_tolower, dbx_tolower, MAP};
+    use crate::{dbx_str_lowercase, dbx_lowercase, MAP};
 
     #[test]
     fn examples() {
         assert_eq!(MAP.len(), 893);
-        assert_eq!('a', dbx_tolower('A'));
-        assert_eq!('a', dbx_tolower('a'));
-        assert_eq!('i', dbx_tolower('İ')); // capital dotted I goes to plain ascii i
-        assert_eq!('ⓜ', dbx_tolower('Ⓜ'));
-        assert_eq!('\u{B5}', dbx_tolower('\u{B5}')); // MICRO SIGN
-        assert_eq!('\u{1F80}', dbx_tolower('\u{1F88}')); // GREEK CAPITAL LETTER ALPHA WITH PSILI AND PROSGEGRAMMENI
+        assert_eq!('a', dbx_lowercase('A'));
+        assert_eq!('a', dbx_lowercase('a'));
+        assert_eq!('i', dbx_lowercase('İ')); // capital dotted I goes to plain ascii i
+        assert_eq!('ⓜ', dbx_lowercase('Ⓜ'));
+        assert_eq!('\u{B5}', dbx_lowercase('\u{B5}')); // MICRO SIGN
+        assert_eq!('\u{1F80}', dbx_lowercase('\u{1F88}')); // GREEK CAPITAL LETTER ALPHA WITH PSILI AND PROSGEGRAMMENI
 
-        assert_eq!("hi thére", dbx_str_tolower("Hİ THÉRE"));
+        assert_eq!("hi thére", dbx_str_lowercase("Hİ THÉRE"));
     }
 
     #[test]
@@ -50,11 +71,11 @@ mod test {
         let mut upper_str = String::new();
         let mut lower_str = String::new();
         for (upper, lower) in MAP {
-            assert_eq!(dbx_tolower(*upper), *lower);
-            assert_eq!(dbx_tolower(*lower), *lower);
+            assert_eq!(dbx_lowercase(*upper), *lower);
+            assert_eq!(dbx_lowercase(*lower), *lower);
             upper_str.push(*upper);
             lower_str.push(*lower);
         }
-        assert_eq!(dbx_str_tolower(&upper_str), lower_str);
+        assert_eq!(dbx_str_lowercase(&upper_str), lower_str);
     }
 }
